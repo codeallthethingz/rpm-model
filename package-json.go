ed33ac3cefc4958ff1abd6ee6e5ab7ac80a85f7d
codeallthethingz <howdoicontactwill@gmail.com>
2019-05-13 19:19:43 -0700

    package.json schema

---
+++ package-json.go
@@ -0,0 +1,9 @@
+package main
+
+// PackageJSON defines the structure of the package.json file.
+type PackageJSON struct {
+	Name        string `json:"name,omitempty"`
+	Version     string `json:"version,omitempty"`
+	Description string `json:"description"`
+	License     string `json:"license,omitempty"`
+}
