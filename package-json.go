056e2a604e23c062ea5fdbaf2add0ec0091254d5
codeallthethingz <howdoicontactwill@gmail.com>
2019-05-18 16:52:43 -0700

    added example and test

--- package-json.go
+++ package-json.go
@@ -2,8 +2,44 @@ package model
 
 // PackageJSON defines the structure of the package.json file.
 type PackageJSON struct {
-	Name        string `json:"name,omitempty"`
-	Version     string `json:"version,omitempty"`
-	Description string `json:"description"`
-	License     string `json:"license,omitempty"`
+	Name         string            `json:"name,omitempty"`
+	Version      string            `json:"version,omitempty"`
+	Description  string            `json:"description"`
+	License      string            `json:"license,omitempty"`
+	Units        string            `json:"units,omitempty"`
+	Metedata     []string          `json:"metadata,omitempty"`
+	Bounds       []Bound           `json:"bounds,omitempty"`
+	Joins        []string          `json:"joins,omitempty"`
+	Dependencies map[string]string `json:"dependencies,omitempty"`
+	Manifest     map[string][]CRS  `json:"manifest,omitempty"`
+}
+
+// CRS Coordinates Rotation Scale
+type CRS struct {
+	Coordinates Coordinates `json:"coordinates,omitempty"`
+	Rotation    Coordinates `json:"rotation,omitempty"`
+	Scale       Coordinates `json:"scale,omitempty"`
+}
+
+// Bound defines an area of a 3D object
+type Bound struct {
+	Name         string       `json:"name,omitempty"`
+	BoundingType BoundingType `json:"boundingType,omitempty"`
+	Coordinates  Coordinates  `json:"coordinates,omitempty"`
+}
+
+// Coordinates where is something in 3 dimensions
+type Coordinates struct {
+	X string `json:"x,omitempty"`
+	Y string `json:"y,omitempty"`
+	Z string `json:"z,omitempty"`
+}
+
+// BoundingType holds the info about the maximum dimensions of the object
+type BoundingType struct {
+	Name         string            `json:"name,omitempty"`
+	Measurements map[string]string `json:"measurements,omitempty"`
+	ScalesX      bool              `json:"scalesX,omitempty"`
+	ScalesY      bool              `json:"scalesY,omitempty"`
+	ScalesZ      bool              `json:"scalesZ,omitempty"`
 }
---
+++ package-json_test.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreate(t *testing.T) {
+	contents, err := ioutil.ReadFile("example-package.json")
+	require.Nil(t, err)
+
+	var p PackageJSON
+	require.Nil(t, json.Unmarshal(contents, &p))
+
+	data, err := json.MarshalIndent(&p, "", "  ")
+	require.Nil(t, err)
+
+	require.Equal(t, "round-bolt", p.Name)
+	require.Equal(t, "0.0.1", p.Version)
+	require.Equal(t, "simple round-bolt 1.5mm pitch", p.Description)
+	require.Equal(t, "MIT", p.License)
+	require.Equal(t, "mm", p.Units)
+	require.Equal(t, []string{"flat-head", "15mm"}, p.Metedata)
+
+	require.Equal(t, 3, len(p.Bounds))
+	bounds0 := p.Bounds[0]
+	require.Equal(t, "total-area", bounds0.Name)
+	require.Equal(t, "total-area", bounds0.Name)
+	require.Equal(t, "round-cylinder", bounds0.BoundingType.Name)
+	require.Equal(t, "0", bounds0.Coordinates.X)
+	require.Equal(t, "0", bounds0.Coordinates.Y)
+	require.Equal(t, "0", bounds0.Coordinates.Z)
+	require.Equal(t, false, bounds0.BoundingType.ScalesX)
+	require.Equal(t, false, bounds0.BoundingType.ScalesY)
+	require.Equal(t, false, bounds0.BoundingType.ScalesZ)
+	require.Equal(t, "15", bounds0.BoundingType.Measurements["diameter"])
+	require.Equal(t, "20", bounds0.BoundingType.Measurements["height"])
+
+	require.Equal(t, "shaft", p.Joins[0])
+	require.Equal(t, "0.0.3", p.Dependencies["flat-head-screwdriver-hole"])
+	screwdriverHole := p.Manifest["flat-head-screwdriver-hole"]
+	require.Equal(t, "0", screwdriverHole[0].Coordinates.X)
+	require.Equal(t, "0", screwdriverHole[0].Coordinates.Y)
+	require.Equal(t, "18", screwdriverHole[0].Coordinates.Z)
+	require.Equal(t, "0", screwdriverHole[0].Rotation.X)
+	require.Equal(t, "0", screwdriverHole[0].Rotation.Y)
+	require.Equal(t, "0", screwdriverHole[0].Rotation.Z)
+	require.Equal(t, "1", screwdriverHole[0].Scale.X)
+	require.Equal(t, "1", screwdriverHole[0].Scale.Y)
+	require.Equal(t, "1", screwdriverHole[0].Scale.Z)
+
+}
